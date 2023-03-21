5cd9ac0df76094ad0090cf4d7d6c65962af24f7e
tungnthust <55620574+tungnthust@users.noreply.github.com>
2023-03-21 16:39:56 +0700

    Update bratsloader.py

--- guided_diffusion/bratsloader.py
+++ guided_diffusion/bratsloader.py
@@ -1,6 +1,7 @@
-import torch
+import torch 
 import torch.nn
 import numpy as np
+import random
 import os
 import os.path
 import nibabel
@@ -35,8 +36,11 @@ class BRATSDataset(torch.utils.data.Dataset):
                 datapoint = dict()
                 # extract all files as channels
                 for f in files:
-                    seqtype = f.split('_')[3]
-                    datapoint[seqtype] = os.path.join(root, f)
+                    try:
+                        seqtype = f.split('_')[3].split('.')[0]
+                        datapoint[seqtype] = os.path.join(root, f)
+                    except:
+                        datapoint['seg'] = os.path.join(root, f)
                 assert set(datapoint.keys()) == self.seqtypes_set, \
                     f'datapoint is incomplete, keys are {datapoint.keys()}'
                 self.database.append(datapoint)
@@ -44,10 +48,13 @@ class BRATSDataset(torch.utils.data.Dataset):
     def __getitem__(self, x):
         out = []
         filedict = self.database[x]
+        slice_index = random.randint(0, 154)
         for seqtype in self.seqtypes:
             number=filedict['t1'].split('/')[4]
             nib_img = nibabel.load(filedict[seqtype])
-            out.append(torch.tensor(nib_img.get_fdata()))
+            full_img = torch.tensor(nib_img.get_fdata())
+            slice_img = full_img[:, :, slice_index]
+            out.append(slice_img)
         out = torch.stack(out)
         out_dict = {}
         if self.test_flag:
@@ -67,18 +74,16 @@ class BRATSDataset(torch.utils.data.Dataset):
             out_dict["y"]=weak_label
         else:
             image = torch.zeros(4,256,256)
+            label = torch.zeros(1,256,256)
             image[:,8:-8,8:-8]=out[:-1,...]		#pad to a size of (256,256)
-            label = out[-1, ...][None, ...]
+            label[:,8:-8,8:-8]=out[-1, ...]
             if label.max()>0:
                 weak_label=1
             else:
                 weak_label=0
             out_dict["y"] = weak_label
 
-        return (image, out_dict, weak_label, label, number )
+        return (image, out_dict, weak_label, label, number)
 
     def __len__(self):
         return len(self.database)
-
-
-
