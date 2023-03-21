0b265104e1131f5ffa41058b8b074900bb25a20b
tungnthust <thanhtungbka1@gmail.com>
2023-03-21 19:58:24 +0700

    update dataloader

--- guided_diffusion/bratsloader.py
+++ guided_diffusion/bratsloader.py
@@ -17,7 +17,7 @@ class BRATSDataset(torch.utils.data.Dataset):
         paths = [p for p in Path(f'{directory}').glob(f'**/*.h5')]
         self.datapaths = []
         for path in paths:
-            volume_idx = int(str(paths[0]).split('/')[-1].split('_')[1])
+            volume_idx = int(str(path).split('/')[-1].split('_')[1])
             if mode == "val" and volume_idx >= 295:
                 self.datapaths.append(path)
             if mode == "train" and volume_idx < 295:
