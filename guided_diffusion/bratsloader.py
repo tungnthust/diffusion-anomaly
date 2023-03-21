734932dd5e6628193f52803dc30a10d2612ec221
tungnthust <thanhtungbka1@gmail.com>
2023-03-21 19:01:20 +0700

    update dataloader

--- guided_diffusion/bratsloader.py
+++ guided_diffusion/bratsloader.py
@@ -25,7 +25,7 @@ class BRATSDataset(torch.utils.data.Dataset):
         label = 1 if np.sum(mask) > 0 else 0
         padding_image = np.zeros((4,256,256)) + image[0][0][0]
         padding_image[:,8:-8,8:-8] = image
-        return padding_image, label
+        return np.float32(padding_image), label
 
     def __len__(self):
         return len(self.datapaths)
