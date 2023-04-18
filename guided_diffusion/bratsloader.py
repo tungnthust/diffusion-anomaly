0d811a3a2761699196b65771134546c2635d000f
tungnthust <thanhtungbka1@gmail.com>
2023-04-18 23:50:15 +0700

    update data loader

--- guided_diffusion/bratsloader.py
+++ guided_diffusion/bratsloader.py
@@ -1,47 +1,55 @@
 import torch 
-import torch.nn
 import numpy as np
-import random
-import os
-import os.path
-import nibabel
-from scipy import ndimage
-import h5py
-from pathlib import Path
-from sklearn.preprocessing import MinMaxScaler
+import torch.nn.functional as F
 import pickle
+
+def normalize(image):
+    """Basic min max scaler.
+    """
+    min_ = np.min(image)
+    max_ = np.max(image)
+    scale = max_ - min_
+    image = (image - min_) / scale
+    return image
+
+def irm_min_max_preprocess(image, low_perc=1, high_perc=99):
+    """Main pre-processing function used for the challenge (seems to work the best).
+    Remove outliers voxels first, then min-max scale.
+    Warnings
+    --------
+    This will not do it channel wise!!
+    """
+
+    non_zeros = image > 0
+    if non_zeros.sum() > 0:
+        low, high = np.percentile(image[non_zeros], [low_perc, high_perc])
+        image = np.clip(image, low, high)
+        image = normalize(image)
+    return image
 
 class BRATSDataset(torch.utils.data.Dataset):
     def __init__(self, directory, mode="train", test_flag=False):
         
         super().__init__()
-        
-#         paths = [p for p in Path(f'{directory}').glob(f'**/*.h5')]
-        paths = []
-        with open('/kaggle/working/diffusion-anomaly/data/data_paths.pickle', 'rb') as fp:
-            paths = pickle.load(fp)
         self.datapaths = []
-        for path in paths:
-            volume_idx = int(str(path).split('/')[-1].split('_')[1])
-            slice_idx = int(str(path).split('/')[-1].split('_')[3].split('.')[0])
-            if (mode == "val") and (volume_idx > 52 and volume_idx <= 88) and (slice_idx >= 80 and slice_idx <= 128):
-                self.datapaths.append(path)
-            if (mode == "train") and (volume_idx <= 52 or volume_idx > 88) and (slice_idx >= 80 and slice_idx <= 128):
-                self.datapaths.append(path)
+        with open(f'/kaggle/working/diffusion-anomaly/data/brats/{mode}_brats20_datapaths.pickle', 'rb') as fp:
+            self.datapaths = pickle.load(fp)
 
     def __getitem__(self, idx):
-        scaler = MinMaxScaler()
-        data = h5py.File(self.datapaths[idx], 'r')
-        image = np.array(data['image'])
-        mask = np.array(data['mask'])
-        image = scaler.fit_transform(image.reshape(-1, image.shape[-1])).reshape(image.shape)
-        image = np.transpose(image, [2, 0, 1])
+        data = np.load(self.datapaths[idx])
+        image = data['image']
+        image = image[[1, 2, 3, 0], :, :]
+        for i in range(image.shape[0]):
+            image[i] = irm_min_max_preprocess(image[i])
+        mask = data['mask']
+        padding_image = np.zeros((4, 256, 256))
+        padding_image[:, 8:-8, 8:-8] = image
+        padding_mask = np.zeros((256, 256))
+        padding_mask[8:-8, 8:-8] = mask
         label = 1 if np.sum(mask) > 0 else 0
-        padding_image = np.zeros((4,256,256)) + np.broadcast_to(image[:, 0, 0][:, np.newaxis, np.newaxis], (4, 256, 256))
-        padding_image[:,8:-8,8:-8] = image
         cond = {}
         cond['y'] = label
-        return np.float32(padding_image), cond, label, np.float32(np.sum(mask, axis=2))
+        return np.float32(padding_image), cond, label, np.float32(padding_mask)
 
     def __len__(self):
         return len(self.datapaths)
