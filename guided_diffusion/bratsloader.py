1bc4eb4484c05a3078a19399ac4f115660552366
tungnthust <thanhtungbka1@gmail.com>
2023-04-18 12:57:14 +0700

    add data preprocess

--- guided_diffusion/bratsloader.py
+++ guided_diffusion/bratsloader.py
@@ -2,7 +2,30 @@ import torch
 import numpy as np
 import torch.nn.functional as F
 import pickle
-import cv2
+
+def normalize(image):
+    """Basic min max scaler.
+    """
+    min_ = np.min(image)
+    max_ = np.max(image)
+    scale = max_ - min_
+    image = (image - min_) / scale
+    return image
+
+def irm_min_max_preprocess(image, low_perc=1, high_perc=99):
+    """Main pre-processing function used for the challenge (seems to work the best).
+    Remove outliers voxels first, then min-max scale.
+    Warnings
+    --------
+    This will not do it channel wise!!
+    """
+
+    non_zeros = image > 0
+    if non_zeros.sum() > 0:
+        low, high = np.percentile(image[non_zeros], [low_perc, high_perc])
+        image = np.clip(image, low, high)
+        image = normalize(image)
+    return image
 
 class BRATSDataset(torch.utils.data.Dataset):
     def __init__(self, directory, mode="train", test_flag=False):
@@ -15,6 +38,8 @@ class BRATSDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         data = np.load(self.datapaths[idx])
         image = data['image']
+        for i in range(image.shape[0]):
+            image[i] = irm_min_max_preprocess(image[i])
         mask = data['mask']
         label = 1 if np.sum(mask) > 0 else 0
         cond = {}
