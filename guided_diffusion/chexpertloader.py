51ca8bd7b31b7aa107f7b9a5feb4d9ae5cc3bd0a
tungnthust <thanhtungbka1@gmail.com>
2023-05-19 09:52:48 +0700

    add chexpert dataloader

--- guided_diffusion/chexpertloader.py
+++ guided_diffusion/chexpertloader.py
@@ -34,7 +34,7 @@ class CheXpertDataset(torch.utils.data.Dataset):
         super().__init__()
         self.datapaths = []
         self.mode = mode
-        self.datapaths = glob.glob(f'\kaggle\input\chexpert-data\chexpert-data\{mode}\*.npz')
+        self.datapaths = glob.glob(f'/kaggle/input/chexpert-data/chexpert-data/{mode}/*.npz')
         print(f"Number data: {len(self.datapaths)}")
 
     def __getitem__(self, idx):
