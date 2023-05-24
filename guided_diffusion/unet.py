5ca231ab9513064a1ba75c8d3c46dd70852a38c7
tungnthust <thanhtungbka1@gmail.com>
2023-05-25 01:22:22 +0700

    add diff-scm code

--- guided_diffusion/unet.py
+++ guided_diffusion/unet.py
@@ -133,67 +133,11 @@ class Downsample(nn.Module):
         else:
             assert self.channels == self.out_channels
             self.op = avg_pool_nd(dims, kernel_size=stride, stride=stride)
-        
-        # super().__init__()
-        # out_channels = out_channels if out_channels else channels
-        # self.dwt = DWT_2D("haar")
-
-        # self.weight = nn.Parameter(th.zeros(out_channels, channels * 4, 3, 3))
-        # self.weight.data = default_init()(self.weight.data.shape)
-        # self.bias = nn.Parameter(th.zeros(out_channels))
-
-        # self.dwt = DWT_2D("haar")
 
     def forward(self, x):
         assert x.shape[1] == self.channels
         return self.op(x)
 
-        # xLL, xLH, xHL, xHH = self.dwt(x)
-
-        # x = th.cat((xLL, xLH, xHL, xHH), dim=1) / 2.0
-
-        # x = F.conv2d(x, self.weight, stride=1, padding=1)
-        # x = x + self.bias.reshape(1, -1, 1, 1)
-        # return x
-    
-def variance_scaling(scale, mode, distribution,
-                     in_axis=1, out_axis=0,
-                     dtype=th.float32,
-                     device='cpu'):
-    """Ported from JAX. """
-
-    def _compute_fans(shape, in_axis=1, out_axis=0):
-        receptive_field_size = np.prod(shape) / shape[in_axis] / shape[out_axis]
-        fan_in = shape[in_axis] * receptive_field_size
-        fan_out = shape[out_axis] * receptive_field_size
-        return fan_in, fan_out
-
-    def init(shape, dtype=dtype, device=device):
-        fan_in, fan_out = _compute_fans(shape, in_axis, out_axis)
-        if mode == "fan_in":
-            denominator = fan_in
-        elif mode == "fan_out":
-            denominator = fan_out
-        elif mode == "fan_avg":
-            denominator = (fan_in + fan_out) / 2
-        else:
-            raise ValueError(
-                "invalid mode for variance scaling initializer: {}".format(mode))
-        variance = scale / denominator
-        if distribution == "normal":
-            return th.randn(*shape, dtype=dtype, device=device) * np.sqrt(variance)
-        elif distribution == "uniform":
-            return (th.rand(*shape, dtype=dtype, device=device) * 2. - 1.) * np.sqrt(3 * variance)
-        else:
-            raise ValueError("invalid distribution for variance scaling initializer")
-
-    return init
-
-
-def default_init(scale=1.):
-    """The same initialization used in DDPM."""
-    scale = 1e-10 if scale == 0 else scale
-    return variance_scaling(scale, 'fan_avg', 'uniform')
 
 class ResBlock(TimestepBlock):
     """
@@ -500,7 +444,7 @@ class UNetModel(nn.Module):
         use_scale_shift_norm=False,
         resblock_updown=False,
         use_new_attention_order=False,
-        image_level_cond=False,
+        image_level_cond=False
     ):
         super().__init__()
 
