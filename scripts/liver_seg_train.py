e9dd4de5a0776aaae693b95fdab361d603498139
tungnthust <thanhtungbka1@gmail.com>
2023-06-13 18:26:00 +0700

    add eval metric

--- scripts/liver_seg_train.py
+++ scripts/liver_seg_train.py
@@ -235,10 +235,11 @@ def main():
 
                     train_losses = []
                     train_dices = []
-                        
+
                     model.train()
 
         if not step % args.log_interval:
+            print(train_dices)
             print('step', step + resume_step)
             logger.dumpkvs()
         if (
